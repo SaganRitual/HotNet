1b4b8ea1fa8a6eb0a228320678448e026d810a31
Rob Bishop <c.sagan.ritual@gmail.com>
2021-01-19 21:03:15 -0800

    Factor cnn code into layer class

--- Hotnet/HotLayerCnn.swift
+++ Hotnet/HotLayerCnn.swift
@@ -47,86 +47,122 @@ class HotLayerCnn {
     let cNeuronsIn: Int
     let cNeuronsOut: Int
 
-    let dataSource: DataSourceCnn
-
     weak var device: MTLDevice!
     let commandQueue: MTLCommandQueue
 
     let filter: MPSCNNFullyConnected
-    weak var outputImage: MPSImage!
+    let inputImage: MPSImage
+    let outputImage: MPSImage
+
+    var outputBuffer: UnsafeMutableRawPointer
 
     init(
+        device: MTLDevice,
         cNeuronsIn: Int, cNeuronsOut: Int,
         weights: [Float], biases: [Float],
-        device: MTLDevice, outputImage: MPSImage
+        outputBuffer: UnsafeMutableRawPointer,
+        activationFunction: MPSCNNNeuron? = MPSCNNNeuronTanH()
     ) {
         self.cNeuronsIn = cNeuronsIn
         self.cNeuronsOut = cNeuronsOut
-        self.outputImage = outputImage
         self.device = device
+        self.outputBuffer = outputBuffer
 
         self.commandQueue = device.makeCommandQueue()!
 
-        let convolutionDescriptor = MPSCNNConvolutionDescriptor(
+        let descriptor = MPSCNNConvolutionDescriptor(
             kernelWidth: 1, kernelHeight: 1,
-            inputFeatureChannels: cNeuronsIn, outputFeatureChannels: cNeuronsOut,
-            neuronFilter: nil
+            inputFeatureChannels: cNeuronsIn,
+            outputFeatureChannels: cNeuronsOut,
+            neuronFilter: activationFunction
         )
 
-        self.dataSource = DataSourceCnn(
+        let dataSource = DataSourceCnn(
             weights: weights, biases: biases,
-            convolutionDescriptor: convolutionDescriptor
+            convolutionDescriptor: descriptor
         )
 
         self.filter = MPSCNNFullyConnected(device: device, weights: dataSource)
+
+        // The fully-connected layer does not seem to like .float32 as input,
+        // so we'll use .float16. This does mean we have to convert our data
+        // from 32-bits to 16-bit floats. For output, it seems float32 is OK.
+        let inputImgDesc = MPSImageDescriptor(
+            channelFormat: .float16, width: 1, height: 1, featureChannels: cNeuronsIn
+        )
+
+        let outputImgDesc = MPSImageDescriptor(
+            channelFormat: .float32, width: 1, height: 1, featureChannels: cNeuronsOut
+        )
+
+        self.inputImage = MPSImage(device: device, imageDescriptor: inputImgDesc)
+        self.outputImage = MPSImage(device: device, imageDescriptor: outputImgDesc)
+    }
+
+    func activate(inputBuffer: [Float]) {
+        activate(inputBuffer: inputBuffer, waitForResult: true)
     }
 
     func activate(
         inputBuffer: UnsafeRawPointer,
-        _ onComplete: @escaping () -> Void
+        _ onComplete: (() -> Void)? = nil,
+        waitForResult: Bool = false
     ) {
-        let inputImage = convertInputToTexture(inputBuffer)
+        let ib = inputBuffer.bindMemory(to: Float.self, capacity: cNeuronsIn)
+        let ic = UnsafeBufferPointer(start: ib, count: cNeuronsIn)
+        let input16 = Bloat16.floats_to_float16s(values: ic.map { $0 })
+        
+        input16.withUnsafeBufferPointer { ptr in
+          for i in 0..<inputImage.texture.arrayLength {
+            let region = MTLRegion(origin: MTLOriginMake(0, 0, 0), size: MTLSizeMake(1, 1, 1))
+            inputImage.texture.replace(
+                region: region, mipmapLevel: 0, slice: i,
+                withBytes: ptr.baseAddress!.advanced(by: i * 4),
+                bytesPerRow: MemoryLayout<UInt16>.stride * 4, bytesPerImage: 0
+            )
+          }
+        }
 
+        let commandQueue = device.makeCommandQueue()!
         let commandBuffer = commandQueue.makeCommandBuffer()!
-//        inputImage.synchronize(on: commandBuffer)
 
         filter.encode(
             commandBuffer: commandBuffer, sourceImage: inputImage,
             destinationImage: outputImage
         )
 
-        commandBuffer.addCompletedHandler { _ in onComplete() }
         commandBuffer.commit()
-    }
-}
 
-private extension HotLayerCnn {
-    func convertInputToTexture(_ input: UnsafeRawPointer) -> MPSImage {
-        let imageDescriptor = MPSImageDescriptor(
-            channelFormat: .float16, width: 1, height: 1,
-            featureChannels: cNeuronsOut
-        )
-
-        let inputImage = MPSImage(
-            device: device, imageDescriptor: imageDescriptor
-        )
-
-        let t = input.bindMemory(to: Float.self, capacity: cNeuronsIn)
-        let f = UnsafeBufferPointer(start: t, count: cNeuronsIn)
-
-        for i in 0..<inputImage.texture.arrayLength {
-            let region = MTLRegion(
-                origin: MTLOriginMake(0, 0, 0), size: MTLSizeMake(1, 1, 1)
-            )
-
-            inputImage.texture.replace(
-                region: region, mipmapLevel: 0, slice: i,
-                withBytes: f.baseAddress!.advanced(by: i * 4),
-                bytesPerRow: MemoryLayout<Float>.stride * 4,
-                bytesPerImage: 0
-            )
+        if waitForResult {
+            commandBuffer.waitUntilCompleted()
+            getActivationResult()
+        } else {
+            commandBuffer.addCompletedHandler { _ in
+                self.getActivationResult()
+                onComplete!()
+            }
         }
+    }
 
-        return inputImage
+    private func getActivationResult() {
+        let width = outputImage.width, height = outputImage.height
+        let fc = outputImage.featureChannels, texture = outputImage.texture
+        let count =  width * height * fc
+        let numSlices = (fc + 3)/4
+
+        let region = MTLRegion(origin: MTLOrigin(x: 0, y: 0, z: 0),
+                               size: MTLSize(width: width, height: height, depth: 1))
+
+        let t = outputBuffer.assumingMemoryBound(to: Float.self)
+        let f = UnsafeMutableBufferPointer(start: t, count: count)
+
+        for i in 0..<numSlices {
+          texture.getBytes(&(f[width * height * 4 * i]),
+                           bytesPerRow: width * 4 * MemoryLayout<Float>.size,
+                           bytesPerImage: 0,
+                           from: region,
+                           mipmapLevel: 0,
+                           slice: i)
+        }
     }
 }
--- Hotnet/main.swift
+++ Hotnet/main.swift
@@ -42,8 +42,11 @@ func bnn() {
     )
 
     let layer = HotLayerBnn(
-        cNeuronsIn: 4, cNeuronsOut: 4, weights: [Float](repeating: 1, count: 16), biases: [Float](repeating: 0, count: 4),
-        outputBuffer: pOutput, activationFunction: .init(function: .identity)
+        cNeuronsIn: 4, cNeuronsOut: 4,
+        weights: [Float](repeating: 1, count: 16),
+        biases: [Float](repeating: 0, count: 4),
+        outputBuffer: pOutput,
+        activationFunction: .init(function: .identity)
     )
 
     layer.activate(inputBuffer: [Float]([1, 2, 3, 4]))
@@ -51,105 +54,32 @@ func bnn() {
     let t = pOutput.assumingMemoryBound(to: Float.self)
     let f = UnsafeBufferPointer(start: t, count: 4)
 
-    print("layer result \(f.map { $0 })")
+    print("bnn layer result \(f.map { $0 })")
 }
 
 func cnn() {
     let gpus = MTLCopyAllDevices()
     let device = gpus[1]
 
-    let cNeuronsIn = 4, cNeuronsOut = 4
-
-    let weights = [Float](repeating: 1, count: cNeuronsIn * cNeuronsOut)
-    let biases = [Float](repeating: 0, count: cNeuronsOut)
-
-    let descriptor = MPSCNNConvolutionDescriptor(
-        kernelWidth: 1, kernelHeight: 1,
-        inputFeatureChannels: cNeuronsIn,
-        outputFeatureChannels: cNeuronsOut,
-        neuronFilter: nil
+    let pOutput = UnsafeMutableRawPointer.allocate(
+        byteCount: 4 * MemoryLayout<Float>.size,
+        alignment: MemoryLayout<Float>.alignment
     )
 
-    let dataSource = DataSourceCnn(
-        weights: weights, biases: biases,
-        convolutionDescriptor: descriptor
+    let layer = HotLayerCnn(
+        device: device,
+        cNeuronsIn: 4, cNeuronsOut: 4,
+        weights: [Float](repeating: 1, count: 16),
+        biases: [Float](repeating: 0, count: 4),
+        outputBuffer: pOutput, activationFunction: nil
     )
 
-    let filter = MPSCNNFullyConnected(device: device, weights: dataSource)
+    layer.activate(inputBuffer: [Float]([1, 2, 3, 4]))
 
-    // The fully-connected layer does not seem to like .float32 as input,
-    // so we'll use .float16. This does mean we have to convert our data
-    // from 32-bits to 16-bit floats. For output, it seems float32 is OK.
-    let inputImgDesc = MPSImageDescriptor(
-        channelFormat: .float16, width: 1, height: 1, featureChannels: cNeuronsIn
-    )
+    let t = pOutput.assumingMemoryBound(to: Float.self)
+    let f = UnsafeBufferPointer(start: t, count: 4)
 
-    let outputImgDesc = MPSImageDescriptor(
-        channelFormat: .float16, width: 1, height: 1, featureChannels: cNeuronsOut
-    )
-
-    let inputImage = MPSImage(device: device, imageDescriptor: inputImgDesc)
-    let outputImage = MPSImage(device: device, imageDescriptor: outputImgDesc)
-
-    print("inputImage.texture.arrayLength, \(inputImage.texture.arrayLength)")
-
-    let input16 = Bloat16.floats_to_float16s(values: [Float]([1,1,1,1]))
-    input16.withUnsafeBufferPointer { ptr in
-      for i in 0..<inputImage.texture.arrayLength {
-        let region = MTLRegion(origin: MTLOriginMake(0, 0, 0), size: MTLSizeMake(1, 1, 1))
-        inputImage.texture.replace(
-            region: region, mipmapLevel: 0, slice: i,
-            withBytes: ptr.baseAddress!.advanced(by: i * 4),
-            bytesPerRow: MemoryLayout<UInt16>.stride * 4, bytesPerImage: 0
-        )
-      }
-    }
-
-    let commandQueue = device.makeCommandQueue()!
-    let commandBuffer = commandQueue.makeCommandBuffer()!
-
-    filter.encode(
-        commandBuffer: commandBuffer, sourceImage: inputImage,
-        destinationImage: outputImage
-    )
-
-    commandBuffer.commit()
-    commandBuffer.waitUntilCompleted()
-
-    let width = outputImage.width, height = outputImage.height
-    let fc = outputImage.featureChannels, texture = outputImage.texture
-    let count =  width * height * fc
-    let numSlices = (fc + 3)/4
-    var output = [UInt16](repeating: 0, count: count)
-
-    print("output stuff")
-    print("w = \(width), h = \(height), fc = \(fc), count = \(count), ns = \(numSlices)")
-
-    let region = MTLRegion(origin: MTLOrigin(x: 0, y: 0, z: 0),
-                           size: MTLSize(width: width, height: height, depth: 1))
-
-    for i in 0..<numSlices {
-      texture.getBytes(&(output[width * height * 4 * i]),
-                       bytesPerRow: width * 4 * MemoryLayout<UInt16>.size,
-                       bytesPerImage: 0,
-                       from: region,
-                       mipmapLevel: 0,
-                       slice: i)
-    }
-
-    let final = Bloat16.float16s_to_floats(values: output)
-
-    print("cnn layer result \(final)")
-
-    let ininin = [Float]([1, 2, 3, 4])
-    print("ininin \(ininin)")
-
-    let midmidmid = Bloat16.floats_to_float16s(values: ininin)
-    print("midmidmid \(midmidmid)")
-
-    let outoutout = Bloat16.float16s_to_floats(values: midmidmid)
-    print("outoutout \(outoutout)")
-
+    print("cnn layer result \(f.map { $0 })")
     semaphore.signal()
 }
 
